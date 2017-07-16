12dd017399cc63f6e12cb6458dd65d5317f23245
Arun Dalal <arundalal786@gmail.com>
2017-07-16 05:06:50 +0000

    ..

---
+++ ecscluster.tf
@@ -0,0 +1,52 @@
+provider "aws" {
+    access_key = "AKIAJPSK7NMCA7FHEZNQ"
+    secret_key = "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"
+    region = "us-east-1"
+}
+
+resource "aws_ecs_cluster" "ecs" {
+    name = "webcluster"
+}
+
+resource "aws_instance" "ecsnode" {
+    count = 1
+    ami = "ami-04351e12"
+    instance_type = "t2.small"
+    key_name = "arun"
+    vpc_security_group_ids = ["sg-45fab839"]
+    associate_public_ip_address = true
+    iam_instance_profile = "ecsInstanceRole"
+    user_data = "#!/bin/bash\necho ECS_CLUSTER=${aws_ecs_cluster.ecs.name} > /etc/ecs/ecs.config"
+    tags {
+    Name = "ECS CONTAINER SERVICE"
+    Creator = "Terraform"
+    }
+}
+
+resource "aws_db_instance" "wordrds" {
+  allocated_storage    = 10
+  storage_type         = "gp2" 
+  engine               = "mysql"
+  engine_version       = "5.6.27"
+  instance_class       = "db.t2.micro"
+  identifier           = "wordpresrds"
+  name               = "wpdata"
+  username             = "wpuser"
+  password             = "password"
+  parameter_group_name = "default.mysql5.6"
+  vpc_security_group_ids =  ["sg-45fab839"]
+}
+
+resource "aws_ecs_task_definition" "webservice" {
+    family = "webservice"
+    container_definitions = "${file("web.json")}"
+}
+
+
+resource "aws_ecs_service" "wordpress" {
+  name            = "wordpress"
+  cluster         = "${aws_ecs_cluster.ecs.id}"
+  task_definition = "${aws_ecs_task_definition.webservice.arn}"
+  desired_count   = 1
+  
+}
